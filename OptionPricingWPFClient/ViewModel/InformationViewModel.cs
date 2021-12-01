605b326235defea65b72451cd4769b18cba13877
Sami Benabidallah <sami.benabidallah@gmail.com>
2021-12-01 12:46:10 +0100

    adding hyperlink to github

--- OptionPricingWPFClient/ViewModel/InformationViewModel.cs
+++ OptionPricingWPFClient/ViewModel/InformationViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OptionPricingWPFClient.ViewModel
 {
@@ -12,6 +14,19 @@ namespace OptionPricingWPFClient.ViewModel
         public static string AppVersion = $"{Assembly.GetExecutingAssembly().GetName().Version.ToString(3)}";
         public static string Title = "Option Pricing App";
         public static string Author = "Sami";
+        public static string GitHubUrl = "https://github.com/DIGISTRAT-Team/OptionPricingApp";
 
+        public ICommand AccessGitHub { get; set; }
+
+        public InformationViewModel()
+        {
+            this.AccessGitHub = new RelayCommand(OnClickAccessGitHubCommand);
+
+        }
+
+        private void OnClickAccessGitHubCommand()
+        {
+            System.Diagnostics.Process.Start(GitHubUrl);
+        }
     }
 }
